caab31e9242cd8435639fdb078ba72ab6d3fdff7
Filip <xdrakles@gmail.com>
2026-03-25 17:51:14 +0100

    feat: auth config added

--- backend/src/main/java/com/casino/project/config/SecurityConfig.java
+++ backend/src/main/java/com/casino/project/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.casino.project.config;
 
+import com.casino.project.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,7 +16,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // Wyłączamy CSRF (ważne przy API i React)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Pozwalamy na każdy dostęp bez logowania
+                        .anyRequest().permitAll() // Na razie pozwalamy na każdy dostęp bez logowania
                 );
         return http.build();
     }
@@ -29,22 +30,20 @@ public class SecurityConfig {
     public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
         return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
     }
+
     @Bean
-    public org.springframework.security.authentication.AuthenticationProvider authenticationProvider() {
+    public org.springframework.security.authentication.AuthenticationProvider authenticationProvider(UserRepository repository) {
         org.springframework.security.authentication.dao.DaoAuthenticationProvider authProvider =
                 new org.springframework.security.authentication.dao.DaoAuthenticationProvider();
 
-        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setUserDetailsService(userDetailsService(repository));
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
 
     @Bean
-    public org.springframework.security.core.userdetails.UserDetailsService userDetailsService() {
-        return username -> {
-
-            return null;
-        };
+    public org.springframework.security.core.userdetails.UserDetailsService userDetailsService(UserRepository repository) {
+        return username -> repository.findByUsername(username)
+                .orElseThrow(() -> new org.springframework.security.core.userdetails.UsernameNotFoundException("Nie znaleziono użytkownika: " + username));
     }
 }
-
