d26ae7b7d7fba5980753629ff1138aa411e498d7
Filip <xdrakles@gmail.com>
2026-04-08 19:55:23 +0200

    feat: add wallet and auth services, update user service

--- backend/src/main/java/com/casino/project/service/WalletService.java
+++ backend/src/main/java/com/casino/project/service/WalletService.java
@@ -1,4 +1,35 @@
 package com.casino.project.service;
 
+import com.casino.project.model.Wallet;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import com.casino.project.repository.WalletRepository;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
 public class WalletService {
+    private final WalletRepository walletRepository;
+
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    public BigDecimal getBalanceByUsername(String username) {
+        Wallet wallet = getWalletEntityByUsername(username);
+
+        return wallet.getBalance();
+    }
+    public Wallet getWalletEntityByUsername(String username) {
+        return walletRepository.findByUserUsername(username)
+                .orElseThrow(() -> new RuntimeException("Nie znaleziono portfela dla użytkownika: " + username));
+    }
+    @Transactional
+    public BigDecimal deposit(BigDecimal amount, String username) {
+        Wallet wallet = getWalletEntityByUsername(username);
+        wallet.setBalance(wallet.getBalance().add(amount));
+        walletRepository.save(wallet);
+        return wallet.getBalance();
+    }
 }
