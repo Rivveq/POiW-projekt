82f730afbfb99a6bd0b1fcd3200acccbd8804a79
Filip <xdrakles@gmail.com>
2026-04-12 12:16:00 +0200

    fix: lombok annotations fix

--- backend/src/main/java/com/casino/project/model/Wallet.java
+++ backend/src/main/java/com/casino/project/model/Wallet.java
@@ -1,19 +1,23 @@
 package com.casino.project.model;
 
-import com.casino.project.model.User;
 import jakarta.persistence.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "wallets")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Wallet {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //One to one relation
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
@@ -21,23 +25,15 @@ public class Wallet {
     @Column(nullable = false, precision = 19, scale = 4)
     private BigDecimal balance = BigDecimal.ZERO;
 
-    public Long getId() {
-        return id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Wallet)) return false;
+        return id != null && id.equals(((Wallet) o).getId());
     }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
