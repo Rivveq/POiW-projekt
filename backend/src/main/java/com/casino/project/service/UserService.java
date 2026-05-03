02f79ad9ca18934c7eb8b325add3d00eacf0993a
Filip <paczula.filip21@gmail.com>
2026-05-03 19:54:31 +0200

    fix: fixed missing field in room service and deleted login handling in user service

--- backend/src/main/java/com/casino/project/service/UserService.java
+++ backend/src/main/java/com/casino/project/service/UserService.java
@@ -1,13 +1,14 @@
 package com.casino.project.service;
 
-import com.casino.project.dto.user.UserLoginRequest;
 import com.casino.project.dto.user.UserRegistrationRequest;
 import com.casino.project.dto.user.UserResponse;
+import com.casino.project.exception.UserAlreadyExistsException;
 import com.casino.project.model.Role;
 import com.casino.project.model.User;
 import com.casino.project.model.Wallet;
 import com.casino.project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +25,7 @@ public class UserService {
     @Transactional
     public UserResponse registerUser(UserRegistrationRequest request) {
         if (userRepository.findByUsername(request.username()).isPresent()) {
-            throw new RuntimeException("User already exists!");
+            throw new UserAlreadyExistsException(request.username());
         }
 
         User user = new User();
@@ -43,20 +44,10 @@ public class UserService {
 
     public UserResponse getUserResponseByUsername(String username) {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
         return mapToResponse(user);
     }
 
-    public UserResponse loginUser(UserLoginRequest request) {
-        User user = userRepository.findByUsername(request.username())
-                .orElseThrow(() -> new RuntimeException("Invalid username or password"));
-
-        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
-            throw new RuntimeException("Invalid username or password");
-        }
-
-        return mapToResponse(user);
-    }
     private UserResponse mapToResponse(User user) {
         return new UserResponse(
                 user.getId(),
