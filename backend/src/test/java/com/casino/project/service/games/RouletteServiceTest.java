9fadbf9ba7a35673f79de68dc2031c4ea6224a46
Pabis1 <pawellwolnyy@gmail.com>
2026-05-20 21:35:06 +0200

    naprawa testu (nie ma błędów elo)

--- backend/src/test/java/com/casino/project/service/games/RouletteServiceTest.java
+++ backend/src/test/java/com/casino/project/service/games/RouletteServiceTest.java
@@ -1,5 +1,6 @@
 package com.casino.project.service.games;
 
+import com.casino.project.dto.wallet.WalletResponse;
 import com.casino.project.service.WalletService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
 import java.security.SecureRandom;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -37,20 +39,20 @@ class RouletteServiceTest {
     void shouldDoubleTheBetWhenWinningOnRed() {
         // given
         String username = "gracz";
-        BigDecimal bet = new BigDecimal("50.00");
+        Map<String, Integer> bets = Map.of("red", 50);
 
         when(secureRandomMock.nextInt(37)).thenReturn(1); // wymuszamy czerwony
+        when(walletService.getBalance(username)).thenReturn(new WalletResponse(new BigDecimal("150.00")));
 
         // when
-        RouletteService.RouletteResult result = rouletteService.play(username, bet, "RED", null);
+        RouletteService.RouletteResult result = rouletteService.play(username, bets);
 
         // then
-        verify(walletService, times(1)).withdraw(username, bet);
+        verify(walletService, times(1)).withdraw(username, BigDecimal.valueOf(50));
 
-        BigDecimal expectedWin = new BigDecimal("100.00");
-        verify(walletService, times(1)).deposit(username, expectedWin);
+        verify(walletService, times(1)).deposit(username, BigDecimal.valueOf(100));
 
-        assertEquals("RED", result.color());
-        assertEquals(expectedWin, result.winAmount());
+        assertEquals(1, result.winningNumber());
+        assertEquals(100, result.totalWin());
     }
 }
